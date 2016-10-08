bc627620bb085bb39cdd484f477fe2af346893f0
Andrey <andrey@MacBook-Pro-Andrey.local>
2016-10-08 16:30:32 +0200

    SignUp restyling

--- client/modal/modal-users.js
+++ client/modal/modal-users.js
@@ -1,18 +1,18 @@
 Template.modalUsers.helpers({
 	'enemyUsers': function() {
-		//var currentUser = Meteor.userId();
-		//var friendsId = Meteor.friends.find({userId: currentUser}).map(function(friend) {
-		//	return friend.friendId;
-		//});
-		//console.log(friendId)
-		//var sentRequests = Meteor.requests.find({requesterId: currentUser}).map(function(requester) {
-		//	return requester.userId;
-		//});
-		//var resivedRequests = Meteor.requests.find({userId: currentUser}).map(function(requester) {
-		//	return requester.requesterId;
-		//});
-		//var result =  Meteor.users.find({_id: {$nin: friendsId.concat(currentUser, sentRequests.concat(resivedRequests))} });
-		return Meteor.users.find();
+		var currentUser = Meteor.userId();
+		var friendsId = Meteor.friends.find({userId: currentUser}).map(function(friend) {
+			return friend.friendId;
+		});
+		
+		var sentRequests = Meteor.requests.find({requesterId: currentUser}).map(function(requester) {
+			return requester.userId;
+		});
+		var resivedRequests = Meteor.requests.find({userId: currentUser}).map(function(requester) {
+			return requester.requesterId;
+		});
+		var result =  Meteor.users.find({_id: {$nin: friendsId.concat(currentUser, sentRequests.concat(resivedRequests))} });
+		return result;
 	}
 });
 
@@ -20,6 +20,8 @@ Template.modalUsers.events({
 	'click #addToFriends': function(event) {
 		event.preventDefault();
 		this.requestFriendship();
-		console.log(this)
+		var participant = Meteor.users.findOne({_id:this._id});
+		var conversation = new Conversation().save();
+		conversation.addParticipant(participant);
 	}
 });
